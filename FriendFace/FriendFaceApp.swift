f1bfcc9f05109f61817dffa1bc278980efb9729d
YonYoon <zhzhzh1806@gmail.com>
2024-01-19 11:29:36 +0500

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Zhansen Zhalel on 19.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
