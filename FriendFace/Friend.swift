d36274991287ed02955b5c4326dc01c56eea95f7
YonYoon <zhzhzh1806@gmail.com>
2024-01-19 16:37:25 +0500

    Added protocol and init

--- FriendFace/Friend.swift
+++ FriendFace/Friend.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-class Friend: Codable {
+class Friend: Codable, Identifiable {
     let id: String
     let name: String
+    
+    init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
